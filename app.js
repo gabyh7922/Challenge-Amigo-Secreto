362a85ad6c758151ba24c584d67d5c25318bd496
gaby7922 <gaby7922@gmail.com>
2025-02-13 13:11:12 -0300

    agregando funciones agregarAmigo y sortearAmigo

--- app.js
+++ app.js
@@ -1,1 +1,38 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+//variable vacia que recibe los nombre
+let amigos = [];
+function mostrarAmigos() {
+    const lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    for (let i = 0; i < amigos.length; i++) {
+        lista.innerHTML += `<li>${amigos[i]}</li>`;
+    }
+}
+//funcion que recibe los nombres del sorteo
+function agregarAmigo() {
+    const input = document.getElementById('amigo');
+    const nombre = input.value.trim();
+
+    if (nombre === "") {
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+//inserta los nombres a la varible
+    amigos.push(nombre);
+    mostrarAmigos();
+    input.value = "";
+}
+
+function sortearAmigo() {
+    if (amigos.length === 0) {
+        alert("La lista de amigos está vacía. Agrega al menos un amigo antes de sortear.");
+        return;
+    }
+//sortea los nombres
+    const indiceAleatorio = Math.floor(Math.random() * amigos.length);
+    const amigoSorteado = amigos[indiceAleatorio];
+
+    const resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<li>El amigo secreto es: ${amigoSorteado}</li>`;
+}
+
+mostrarAmigos();
